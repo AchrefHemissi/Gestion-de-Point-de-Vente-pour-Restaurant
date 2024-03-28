b6458282f9336441686e92e00aa247bd8cb8e916
LeithEng <147639906+LeithEng@users.noreply.github.com>
2024-03-28 16:24:49 +0100

    Update admin.php

--- Admin/admin.php
+++ Admin/admin.php
@@ -26,8 +26,12 @@
             <span class="links_name">Customers</span>
           </a>
         </li>
+        <li>
+          <a href="#" onclick="showOrders()">
+          <span class="links_name">Orders</span>
+        </li>
         <li class="log_out" id="logOutButton">
-          <a href="logout.html">
+          <a href="login/index.html">
             <i class="bx bx-log-out"></i>
             <span class="links_name">Log out</span>
           </a>
@@ -51,36 +55,36 @@
           <div class="recent-sales box moving">
             <div class="title">Recent Orders</div>
             <?php 
-            $conn =  mysqli_connect("localhost", "root","", "resto");
-            if (!$conn) {
+            $con = mysqli_connect("sql206.infinityfree.com", "if0_36253541", "yXzYSFMPXEvr", "if0_36253541_glicious");
+            if (!$con) {
               die("Connection failed: " . mysqli_connect_error());
             }
             $sql = "SELECT * FROM commande";
-            $result = mysqli_query($conn, $sql);
+            $result = mysqli_query($con, $sql);
             echo "<ul>";
             while ($row = mysqli_fetch_assoc($result)) {
                 echo "<li>". "Order ID : " . $row['id'] . " - " . "Client ID: ". $row['id_client'] . " - " . "Date: ". $row['date_commande'] . "</li>";
             }
             echo "</ul>";
-            mysqli_close($conn);
+            mysqli_close($con);
             ?>
           </div>
           <div class="top-sales box moving">
             <div class="title">Top Selling Products</div>
             <ul class="top-sales-details">
             <?php 
-            $conn =  mysqli_connect("localhost", "root","", "resto");
-            if (!$conn) {
+            $con = mysqli_connect("sql206.infinityfree.com", "if0_36253541", "yXzYSFMPXEvr", "if0_36253541_glicious");
+            if (!$con) {
               die("Connection failed: " . mysqli_connect_error());
             }
             $sql = "SELECT * FROM produit order by vendu desc limit 3 ";
-            $result = mysqli_query($conn, $sql);
+            $result = mysqli_query($con, $sql);
             echo "<ul>";
             while ($row = mysqli_fetch_assoc($result)) {
                 echo "<li>". $row['name'] ." : ". $row['vendu']. " units". "</li>";
             }
             echo "</ul>";
-            mysqli_close($conn);
+            mysqli_close($con);
             ?>
             </ul>
           </div>
@@ -97,12 +101,12 @@
         style="display: none"
       >
           <?php 
-            $conn =  mysqli_connect("localhost", "root","", "resto");
-            if (!$conn) {
+            $con = mysqli_connect("sql206.infinityfree.com", "if0_36253541", "yXzYSFMPXEvr", "if0_36253541_glicious");
+            if (!$con) {
               die("Connection failed: " . mysqli_connect_error());
             };
               $sql = "SELECT * FROM utilisateur";
-              $result = mysqli_query($conn, $sql);
+              $result = mysqli_query($con, $sql);
             
               while ($row = mysqli_fetch_assoc($result)) {
                   echo '<div class="customer">';
@@ -115,9 +119,29 @@
                     echo '<button class="ban-button" onclick="ban(this)">Ban</button>';
                   echo '</div>';
             }
-            mysqli_close($conn);
+            mysqli_close($con);
             ?>
       </div>
+      <div class="orders-list" id="ordersList" style="display: none"> 
+      <?php 
+            $con = mysqli_connect("sql206.infinityfree.com", "if0_36253541", "yXzYSFMPXEvr", "if0_36253541_glicious");
+            if (!$con) {
+              die("Connection failed: " . mysqli_connect_error());
+            };
+            $sql="SELECT c.id as cid, u.nom as lname, u.prenom as fname, o.id_produit as prod, o.quantite as quan FROM commande c
+                  Join utilisateur u on u.id=c.id_client
+                  Join ordproduit o on o.id_commande=c.id";
+            $res=mysqli_query($con,$sql);
+            while($row=mysqli_fetch_assoc($result))
+            {
+              echo "<li>"."Order Number: ".$row["cid"]." Customer: ".$row["fname"]." ".$row["lname"]." Product ID: ".$row["prod"]." Quantity bought: ".$row["quan"];
+            }
+            mysqli_close($con);
+            ?>
+    </div>
+      
+
+
     </section>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.2/dist/chart.umd.min.js"></script>
     <script src="admin.js"></script>
