ce78aa294b204a32368e9322a130319bd4ee2308
MedSahbiBenKalia <mohamedsahbi.benkalia@insat.ucar.tn>
2024-04-01 10:16:20 +0100

    update cart.php

--- client/cart.php
+++ client/cart.php
@@ -211,37 +211,9 @@ $_SESSION['total'] = $total;
 
 <footer class="footer">
 
-   <section class="box-container">
+   
 
-      <div class="box">
-         <img src="images/email-icon.png" alt="">
-         <h3>our email</h3>
-         <a href="mailto:shaikhanas@gmail.com">shaikhanas@gmail.com</a>
-         <a href="mailto:anasbhai@gmail.com">anasbhai@gmail.com</a>
-      </div>
-
-      <div class="box">
-         <img src="images/clock-icon.png" alt="">
-         <h3>opening hours</h3>
-         <p>00:07am to 00:10pm </p>
-      </div>
-
-      <div class="box">
-         <img src="images/map-icon.png" alt="">
-         <h3>our address</h3>
-         <a href="https://www.google.com/maps">mumbai, india - 400104</a>
-      </div>
-
-      <div class="box">
-         <img src="images/phone-icon.png" alt="">
-         <h3>our number</h3>
-         <a href="tel:1234567890">+123-456-7890</a>
-         <a href="tel:1112223333">+111-222-3333</a>
-      </div>
-
-   </section>
-
-   <div class="credit">&copy; copyright @ 2022 by <span>mr. web designer</span> | all rights reserved!</div>
+   <div class="credit"></div>
 
 </footer>
 
