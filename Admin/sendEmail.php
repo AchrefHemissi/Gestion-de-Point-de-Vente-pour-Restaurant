45146e39a4dddd2a4b63e0ac74f2abc4c12ea80c
Mohammed Achref Hemissi <110178894+AchrefHemissi@users.noreply.github.com>
2024-04-18 21:41:20 +0100

    Update sendEmail.php

--- Admin/sendEmail.php
+++ Admin/sendEmail.php
@@ -22,8 +22,8 @@ try {
     // gmail SMTP server settings
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true; // Enable SMTP authentication
-    $mail->Username =  'the gmail of the company'; 
-    $mail->Password = 'the password of the gmail'; 
+    $mail->Username = 'the gmail of the company';; // Your Gmail address
+    $mail->Password = 'the password of the gmail';  // Your Gmail password
     $mail->Port = 587; // TCP port to connect to
 
     // Sender and recipient
@@ -37,12 +37,70 @@ try {
 
     // Send email
     $mail->send();
-    echo 'Email sent successfully ce lien expire automatiquement après 5 secondes.';
+    $isSent = true;
+    $message = "Email sent successfully.";
 } catch (Exception $e) {
-    echo "Error: {$mail->ErrorInfo}" ;
+    $isSent = false;
+    $message = "Error: {$mail->ErrorInfo}";
 }
-echo "<script>
-setTimeout(function(){
-    window.location.href = 'admin.php';
-}, 5000);
-</script>";
+
+$color = $isSent ? "#ff6d00" : "red";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            text-align: center;
+            padding-top: 50px;
+        }
+
+        .container {
+            max-width: 500px;
+            margin: 0 auto;
+            background-color: <?php echo $color; ?>;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+    </style>
+    <link rel="icon" type="image/png" href="logo.png" class="icone" />
+</head>
+
+<body>
+    <div class="container">
+        <p><?php echo $message; ?></p>
+        <p>This link will expire automatically after <span id="countdown">7</span> seconds.</p>
+
+    </div>
+
+    <script>
+        var count = parseInt(document.getElementById('countdown').innerHTML);
+
+        function countdown() {
+
+            document.getElementById('countdown').innerHTML = count;
+            count--;
+            if (count < 0) {
+
+                window.location.href = 'admin.php';
+            } else {
+
+                setTimeout(countdown, 1000);
+            }
+        }
+
+        countdown();
+    </script>
+</body>
+
+</html>
