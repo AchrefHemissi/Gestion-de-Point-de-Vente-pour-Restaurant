acfee78cc899b15123eb4fd278e414089f2cd09f
Mohammed Achref Hemissi <110178894+AchrefHemissi@users.noreply.github.com>
2024-04-23 18:52:53 +0100

    fix session_check to avoid the user-admin access

--- client/session_check.php
+++ client/session_check.php
@@ -1,7 +1,19 @@
 <?php
+require_once 'connexionAdminDB.php';
+
 
 session_start();
-if (!isset($_SESSION['user_id'])) {
+if (!(isset($_SESSION['user_id']))) {
     header("Location: ../login/index.php");
-    exit;
+} else {
+    $con = Database::getInstance();
+    $query = $con->prepare('SELECT is_admin FROM utilisateur WHERE id = ?');
+    $query->bind_param('i', $_SESSION['user_id']);
+    $query->execute();
+    $result = $query->get_result();
+    $row = $result->fetch_assoc();
+    if ($row['is_admin'] == 1) {
+        header("Location: ../login/index.php");
+    }
 }
+exit;
