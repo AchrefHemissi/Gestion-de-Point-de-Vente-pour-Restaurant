b930bdb41acc93befcb3f4667609e1e7196cdcb2
Mohammed Achref Hemissi <110178894+AchrefHemissi@users.noreply.github.com>
2024-04-23 20:13:50 +0100

    Update session_check.php

--- client/session_check.php
+++ client/session_check.php
@@ -1,19 +1,16 @@
 <?php
-require_once 'connexionAdminDB.php';
+require_once 'connexionBD.php';
 
 
 session_start();
 if (!(isset($_SESSION['user_id']))) {
     header("Location: ../login/index.php");
 } else {
-    $con = Database::getInstance();
+    $con = ConnexionBD::getInstance();
     $query = $con->prepare('SELECT is_admin FROM utilisateur WHERE id = ?');
-    $query->bind_param('i', $_SESSION['user_id']);
-    $query->execute();
-    $result = $query->get_result();
-    $row = $result->fetch_assoc();
+    $query->execute([$_SESSION['user_id']]);
+    $row = $query->fetch(PDO::FETCH_ASSOC);
     if ($row['is_admin'] == 1) {
         header("Location: ../login/index.php");
     }
 }
-exit;
